8658fc1cd573c2193a65b15b96dd6bcc7ab5914f
Timmy <timmy.the.nobody@gmail.com>
2022-05-26 11:02:38 +0200

    Initial commit

---
+++ Shared/Index.lua
@@ -0,0 +1,11 @@
+local sCmd = "luarun"
+
+local function isLuaRun( sInput )
+    return string.sub( sInput, 0, ( #sCmd + 1 ) ) == ( sCmd .. " " )
+end
+
+_ENV[ ( Server and "Server" or "Client" ) ].Subscribe( "Console", function( sText )
+    if ( #sText >= ( #sCmd + 2 ) ) and isLuaRun( sText ) then
+        load( string.sub( sText, ( #sCmd + 2 ), #sText ) )()
+    end
+end )
