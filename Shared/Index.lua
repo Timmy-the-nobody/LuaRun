cb7555e6e3ca47d843548d01d42869669be617ff
Timmy <timmy.the.nobody@gmail.com>
2023-01-05 12:37:46 +0100

    chore: avoid "Available commands" warning

--- Shared/Index.lua
+++ Shared/Index.lua
@@ -1,11 +1,12 @@
 local sCmd = "luarun"
 
-local function isLuaRun( sInput )
-    return string.sub( sInput, 0, ( #sCmd + 1 ) ) == ( sCmd .. " " )
+local function isLuaRun(sInput)
+    return string.sub(sInput, 0, (#sCmd + 1)) == (sCmd.." ")
 end
 
-_ENV[ ( Server and "Server" or "Client" ) ].Subscribe( "Console", function( sText )
-    if ( #sText >= ( #sCmd + 2 ) ) and isLuaRun( sText ) then
-        load( string.sub( sText, ( #sCmd + 2 ), #sText ) )()
+_ENV[(Server and "Server" or "Client")].Subscribe("Console", function(sText)
+    if (#sText >= (#sCmd + 2)) and isLuaRun(sText) then
+        load(string.sub(sText, (#sCmd + 2), #sText))()
+        return false
     end
-end )
+end)
