aefe94be74569606cf61158ee5ee5c6b1fbf2046
Nogitsu <nogitsu.9@gmail.com>
2023-02-02 19:41:06 +0100

    Added better error logging

--- Shared/Index.lua
+++ Shared/Index.lua
@@ -1,3 +1,6 @@
 Console.RegisterCommand("luarun", function(...) -- Each word is an argument
-    load(table.concat({...}, " "))()
+    local success, err = pcall(load(table.concat({...}, " "))) -- Concatenates the code and safely load it
+    if not success then
+        Console.Error(err) -- Log the error to the console if there is any
+    end
 end, "Runs your lua code easily !")
