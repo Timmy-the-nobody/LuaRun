30f48daf982fbf88c6a4860e58c24b1136ac0ca0
Nogitsu <nogitsu.9@gmail.com>
2023-02-02 20:02:24 +0100

    Better error log
    Used xpcall instead of pcall to get the full error traceback using debug.traceback

--- Shared/Index.lua
+++ Shared/Index.lua
@@ -1,6 +1,6 @@
 Console.RegisterCommand("luarun", function(...) -- Each word is an argument
-    local success, err = pcall(load(table.concat({...}, " "))) -- Concatenates the code and safely load it
+    local success, err = xpcall(load(table.concat({...}, " ")), debug.traceback) -- Concatenates the code and safely load it
     if not success then
-        Console.Error(err) -- Log the error to the console if there is any
+        Console.Error(err) -- Log the error's traceback to the console if there is any
     end
 end, "Runs your lua code easily !")
