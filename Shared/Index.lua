5b51b2d48dc2ec0e36f0f8d0daee10915f882f70
Timmy <timmy.the.nobody@gmail.com>
2023-02-06 14:40:20 +0100

    feat!: use `Console.RegisterCommand`, add "lua_openscript" cmd

--- Shared/Index.lua
+++ Shared/Index.lua
@@ -1,12 +1,27 @@
-local sCmd = "luarun"
+local tCommands = {
+    {
+        command = "lua_run",
+        func = function(...)
+            load(table.concat({...}, " "))()
+        end,
+        desc = "Console command that allows you to execute lua code",
+        args = {"Lua string to execute"}
+    },
+    {
+        command = "lua_openscript",
+        func = function(sPath)
+            if not Server then
+                Console.Warn("`lua_openscript` is is only available on the server side")
+                return
+            end
 
-local function isLuaRun(sInput)
-    return sInput:sub(0, (#sCmd + 1)) == (sCmd.." ")
+            Package.Require(sPath)
+        end,
+        desc = "Console command that allows you to run a lua script",
+        args = {"Script path"}
+    }
+}
+
+for _, tCmd in ipairs(tCommands) do
+    Console.RegisterCommand(tCmd.command, tCmd.func, tCmd.desc, tCmd.args)
 end
-
-Console.Subscribe("PlayerSubmit", function(sText)
-    if (#sText >= (#sCmd + 2)) and isLuaRun(sText) then
-        load(sText:sub((#sCmd + 2), #sText))()
-        return false
-    end
-end)
